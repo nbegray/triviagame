3f088809c0e427fb71c8fc926d2572d3511c0815
Natalie Edmonson <natalieedmonson@Natalies-MacBook-Air.local>
2019-01-25 16:27:42 -0700

    trying to get this working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,22 @@
 $(document).ready(function (){
-$("#start").on('click', function () {
+$("#start").on("click", function () {
 
     for (i = 0; i < questions.length; i++)
         $("#quizsub").append("<h3>" + questions[i] + questions + "</h3>")
         {
-        for (var j = 0; j < questions[i].answers.length; j++) 
+        
+        for (var j = 0; j < (questions[i].answers.length); j++) 
         {
 
-            $("#quizsub").append("<input type='radio' name='question-" + i + " 'value= '" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
+            $("#quizsub").append("<input type='radio' name='question-'" + i + " 'value= '" + questions[i].answers[j] + "'>'" + questions[i].answers[j]);
         }
     }
-});
+    });
+
+
 
 var questions = [
+    
     {
         question: "Why am I doing this assignment?",
         answers: ["I don't want to be broke and homeless.", "Tucker said we had to...", "I want the certificate.", "All of the above"],
